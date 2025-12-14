dcacbfa0bc8a89e42a4bcfa69d48ee23e88c560d
JustGod <85418813+JustGodWork@users.noreply.github.com>
2025-12-14 18:59:47 +0100

    fix(user_class): correct formatting in User toString method

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -25,5 +25,5 @@ class User {
   }
 
   @override
-  String toString() => 'User(id : $id ,name: $name,  age: $age, height: $height)';
+  String toString() => 'User(id : $id ,name: $name, age: $age, height: $height)';
 }
