61679b7ee827ba48a56003e628a57d2aa7be622a
JustGod <85418813+JustGodWork@users.noreply.github.com>
2025-12-14 18:50:25 +0100

    feat(password_class): implement Password class with validation and string representation

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,23 @@
+class Password {
+  String _password = '';
+
+  Password({required String password}) : _password = password;
+
+  String get password => _password;
+
+  set password(String value) => _password = value;
+
+  bool isValid() {
+    final len = _password.length;
+    if (len < 8 || len > 16) return false;
+
+    final hasUpper = RegExp(r'[A-Z]').hasMatch(_password);
+    final hasLower = RegExp(r'[a-z]').hasMatch(_password);
+    final hasDigit = RegExp(r'\d').hasMatch(_password);
+
+    return hasUpper && hasLower && hasDigit;
+  }
+
+  @override
+  String toString() => 'Your Password is: $_password';
+}
