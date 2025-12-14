7a6bfd4e239ec876bc34c5d1786cb3fa0f21c515
JustGod <85418813+JustGodWork@users.noreply.github.com>
2025-12-14 19:23:30 +0100

    feat(print_rm_characters): refactor printRmCharacters function to improve error handling and response parsing

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,23 +1,20 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 Future<void> printRmCharacters() async {
+    final url = Uri.https("rickandmortyapi.com", "api/character");
+
     try {
-        final url = Uri.parse('https://rickandmortyapi.com/api/character');
         final response = await http.get(url);
-
-        if (response.statusCode != 200) {
-            print('error caught: ${response.statusCode}');
-            return;
+        final Map<String, dynamic> dedodedResponse =
+            jsonDecode(utf8.decode(response.bodyBytes));
+        final List<dynamic> characterResults = dedodedResponse['results'];
+        for (final name in characterResults) {
+            final String characterName = name['name'];
+            print(characterName);
         }
-
-        final data = jsonDecode(response.body);
-        final results = data['results'];
-
-        for (final character in results) {
-            print(character['name']);
-        }
-    } catch (error) {
-        print('error caught: $error');
+    } catch (err) {
+        print('error caught:$err');
     }
 }
